770d88f0deb4a91cda5e328ac7bcd1ec38092f77
TomPeters <tom@tjpeters.net>
2012-09-16 10:12:20 +1000

    Added example test for an output tab (summary)

---
+++ SolarPowerCalcAndroid/test/au/edu/qut/inn372/greenhat/activity/OutputSummaryActivityTest.java
@@ -0,0 +1,69 @@
+package au.edu.qut.inn372.greenhat.activity;
+
+import java.text.DecimalFormat;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.Intent;
+import au.edu.qut.inn372.greenhat.bean.Calculator;
+
+public class OutputSummaryActivityTest extends
+		ActivityInstrumentationTestCase2<OutputSummaryActivity> {
+	
+	private static final int TIMEOUT = 10000;
+	private static final Double SYSTEM_COST = new Double(45000);
+	private static final Double SYSTEM_SIZE = new Double(4.5);
+	OutputSummaryActivity activity;
+	TabbedOutputActivity parentActivity;
+	Calculator calculator;
+	DecimalFormat df = new DecimalFormat("#.##");
+	
+	public OutputSummaryActivityTest(){
+		super(OutputSummaryActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		calculator = new Calculator();
+		calculator.getEquipment().setSize(SYSTEM_SIZE);
+		calculator.getEquipment().setCost(SYSTEM_COST);
+		
+		//Need to use an activityMonitor to get the activity since we are starting it in a thread
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(OutputSummaryActivity.class.getName(), null, false);
+		
+		//Need to set up the parent as well as the activity depends upon it (otherwise would get null pointer exceptions)
+		Intent tabbedOutputIntent = new Intent();
+		tabbedOutputIntent.putExtra("Calculator", calculator);
+		parentActivity = launchActivityWithIntent("au.edu.qut.inn372.greenhat.activity", TabbedOutputActivity.class, tabbedOutputIntent);
+		activity = (OutputSummaryActivity) activityMonitor.waitForActivityWithTimeout(TIMEOUT);
+	}
+	
+	/**
+	 * Explicitly destroy (finish) the parent tabbed activity to prevent exceptions with multiple tests
+	 */
+	protected void tearDown() throws Exception {
+		parentActivity.finish();
+		super.tearDown();
+	}
+	
+	public void testStartUp(){
+		assertTrue(OutputSummaryActivity.class.getName().length() > 0);
+		assertNotNull(activity);
+	}
+	
+	/**
+	 * Tests that the correct data is displayed
+	 */
+	public void testDisplayData() throws InterruptedException {
+		assertEquals(SYSTEM_SIZE.toString(), ((TextView)activity.findViewById(R.id.TextViewSystemSizeField)).getText().toString());
+		assertEquals("$"+df.format(SYSTEM_COST), ((TextView)activity.findViewById(R.id.TextViewSystemCostField)).getText().toString());
+		//TODO Need to add payback period test
+	}
+	
+}
