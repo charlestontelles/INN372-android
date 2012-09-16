3bb05fd18aa086dc1e96191623deda15ac022c66
TomPeters <tom@tjpeters.net>
2012-09-16 14:17:13 +1000

    Added remaining unit tests for input activities

---
+++ SolarPowerCalcAndroid/test/au/edu/qut/inn372/greenhat/activity/CustomerUsageActivityTest.java
@@ -0,0 +1,141 @@
+package au.edu.qut.inn372.greenhat.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+
+public class CustomerUsageActivityTest extends
+		ActivityInstrumentationTestCase2<CustomerUsageActivity> {
+	
+	private static final int TIMEOUT = 10000;
+	private CustomerUsageActivity activity;
+	private TabbedActivity parentActivity;
+	
+	public CustomerUsageActivityTest(){
+		super(CustomerUsageActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		//Need to use an activityMonitor to get the activity
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(CustomerUsageActivity.class.getName(), null, false);
+		
+		//Need to set up the parent (TabbedActivity) as well as the activity depends upon it (otherwise would get null pointer exceptions)
+		parentActivity = launchActivity("au.edu.qut.inn372.greenhat.activity", TabbedActivity.class, null);
+		
+		activity = (CustomerUsageActivity) activityMonitor.waitForActivityWithTimeout(TIMEOUT);
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		parentActivity.finish(); //Explicitly destroy (finish) the parent tabbed activity to prevent exceptions with multiple tests
+		super.tearDown();
+	}
+	
+	/**
+	 * Tests that the activity starts up correctly
+	 */
+	public void testStartUp(){
+		assertTrue(CustomerUsageActivity.class.getName().length() > 0);
+		assertNotNull(activity);
+	}
+	
+	/**
+	 * Populates fields with appropriate test data
+	 */
+	private void populateTestData() {
+		//Example from basic input activity
+		//((EditText)activity.findViewById(R.id.editRoof_Usage_UsagePerDay)).setText(DAILY_USAGE); 
+		//TODO Add the rest of the input fields
+	}
+	
+	/**
+	 * Test that data is saved when the activity is paused
+	 */
+	public void testSaveData() {
+		populateTestData();
+		final Instrumentation myInstr = getInstrumentation();
+		
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				myInstr.callActivityOnPause(activity); //This 'pauses' the activity which causes the saveData method to be called
+			}
+		});
+		myInstr.waitForIdleSync(); //Wait for the above threaded method to complete
+		
+		//Check that the data used in the populateTestData method has been saved to the bean hierarchy
+		//Example is from basic input activity test:
+		//assertEquals(new Double(parentActivity.getCalculator().getCustomer().getElectricityUsage().getDailyAverageUsage()).toString(), DAILY_USAGE);
+		//TODO Add the rest of the fields
+	}
+	
+	/**
+	 * Test that the data is loaded when the activity is paused
+	 */
+	public void testLoadData() {
+		populateTestData();
+		final Instrumentation myInstr = getInstrumentation();
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				myInstr.callActivityOnPause(activity); //This 'pauses' the activity which causes the saveData method to be called
+			}
+		});
+		myInstr.waitForIdleSync(); //Wait for the above threaded method to complete
+		
+		//Change values here to check that they are loaded from the calculator and not just the same as they were before pausing
+		//Example is from basic input activity test:
+		//Double newDailyUsage = 5.5;
+		//parentActivity.getCalculator().getCustomer().getElectricityUsage().setDailyAverageUsage(newDailyUsage);
+		//TODO add the rest of the fields
+		
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				myInstr.callActivityOnResume(activity); //This 'resumes' the activity which causes the loadData method to be called
+			}
+		});
+		myInstr.waitForIdleSync(); //Wait for the above threaded method to complete
+		
+		//Asserts here for all values to check they are being loaded
+		//Example is from basic input activity test:
+		//assertEquals(((EditText)(activity.findViewById(R.id.editRoof_Usage_UsagePerDay))).getText().toString(), newDailyUsage.toString());
+		//TODO Add the rest of the fields
+	}
+	
+	/**
+	 * Tests that the next button launches the next activity
+	 */
+	public void testNextActivity() {
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(EquipmentActivity.class.getName(), null, false);
+		final Button button = (Button) activity.findViewById(R.id.buttonCustomerUsage_Next);
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				button.performClick();
+			}
+		});
+		Activity nextActivity = activityMonitor.waitForActivityWithTimeout(TIMEOUT);
+		assertNotNull(nextActivity);
+		nextActivity.finish();
+	}
+	
+	
+	/**
+	 * Tests that the logout button login activity
+	 */
+	public void testLogoutActivity() {
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LoginActivity.class.getName(), null, false);
+		final Button button = (Button) activity.findViewById(R.id.buttonCustomerUsage_Back);
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				button.performClick();
+			}
+		});
+		Activity nextActivity = activityMonitor.waitForActivityWithTimeout(TIMEOUT);
+		assertNotNull(nextActivity);
+		nextActivity.finish();
+	}
+}
---
+++ SolarPowerCalcAndroid/test/au/edu/qut/inn372/greenhat/activity/RoofActivityTest.java
@@ -0,0 +1,145 @@
+package au.edu.qut.inn372.greenhat.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+
+public class RoofActivityTest extends
+		ActivityInstrumentationTestCase2<RoofActivity> {
+	
+	private static final int TIMEOUT = 10000;
+	private RoofActivity activity;
+	private TabbedActivity parentActivity;
+	
+	public RoofActivityTest(){
+		super(RoofActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		//Need to set up the parent (TabbedActivity) as well as the activity depends upon it (otherwise would get null pointer exceptions)
+		parentActivity = launchActivity("au.edu.qut.inn372.greenhat.activity", TabbedActivity.class, null);
+		
+		//Need to use an activityMonitor to get the activity since we are starting it in a thread
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(RoofActivity.class.getName(), null, false);
+		parentActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				parentActivity.switchTab(TabbedActivity.ROOF_ID);
+			}
+		});
+		activity = (RoofActivity) activityMonitor.waitForActivityWithTimeout(TIMEOUT);
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		parentActivity.finish(); //Explicitly destroy (finish) the parent tabbed activity to prevent exceptions with multiple tests
+		super.tearDown();
+	}
+	
+	/**
+	 * Tests that the activity starts up correctly
+	 */
+	public void testStartUp(){
+		assertTrue(EquipmentActivity.class.getName().length() > 0);
+		assertNotNull(activity);
+	}
+	
+	/**
+	 * Populates fields with appropriate test data
+	 */
+	private void populateTestData() {
+		//Example from basic input activity
+		//((EditText)activity.findViewById(R.id.editRoof_Usage_UsagePerDay)).setText(DAILY_USAGE); 
+		//TODO Add the rest of the input fields
+	}
+	
+	/**
+	 * Test that data is saved when the activity is paused
+	 */
+	public void testSaveData() {
+		populateTestData();
+		final Instrumentation myInstr = getInstrumentation();
+		
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				myInstr.callActivityOnPause(activity); //This 'pauses' the activity which causes the saveData method to be called
+			}
+		});
+		myInstr.waitForIdleSync(); //Wait for the above threaded method to complete
+		
+		//Check that the data used in the populateTestData method has been saved to the bean hierarchy
+		//Example is from basic input activity test:
+		//assertEquals(new Double(parentActivity.getCalculator().getCustomer().getElectricityUsage().getDailyAverageUsage()).toString(), DAILY_USAGE);
+		//TODO Add the rest of the fields
+	}
+	
+	/**
+	 * Test that the data is loaded when the activity is paused
+	 */
+	public void testLoadData() {
+		populateTestData();
+		final Instrumentation myInstr = getInstrumentation();
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				myInstr.callActivityOnPause(activity); //This 'pauses' the activity which causes the saveData method to be called
+			}
+		});
+		myInstr.waitForIdleSync(); //Wait for the above threaded method to complete
+		
+		//Change values here to check that they are loaded from the calculator and not just the same as they were before pausing
+		//Example is from basic input activity test:
+		//Double newDailyUsage = 5.5;
+		//parentActivity.getCalculator().getCustomer().getElectricityUsage().setDailyAverageUsage(newDailyUsage);
+		//TODO add the rest of the fields
+		
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				myInstr.callActivityOnResume(activity); //This 'resumes' the activity which causes the loadData method to be called
+			}
+		});
+		myInstr.waitForIdleSync(); //Wait for the above threaded method to complete
+		
+		//Asserts here for all values to check they are being loaded
+		//Example is from basic input activity test:
+		//assertEquals(((EditText)(activity.findViewById(R.id.editRoof_Usage_UsagePerDay))).getText().toString(), newDailyUsage.toString());
+		//TODO Add the rest of the fields
+	}
+	
+	/**
+	 * Tests that the next button launches the next activity
+	 */
+	public void testNextActivity() {
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LocationActivity.class.getName(), null, false);
+		final Button button = (Button) activity.findViewById(R.id.buttonRoof_Next);
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				button.performClick();
+			}
+		});
+		Activity nextActivity = activityMonitor.waitForActivityWithTimeout(TIMEOUT);
+		assertNotNull(nextActivity);
+		nextActivity.finish();
+	}
+	
+	
+	/**
+	 * Tests that the back button returns to the previous activity
+	 */
+	public void testLogoutActivity() {
+		ActivityMonitor activityMonitor = getInstrumentation().addMonitor(EquipmentActivity.class.getName(), null, false);
+		final Button button = (Button) activity.findViewById(R.id.buttonRoof_Back);
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				button.performClick();
+			}
+		});
+		Activity nextActivity = activityMonitor.waitForActivityWithTimeout(TIMEOUT);
+		assertNotNull(nextActivity);
+		nextActivity.finish();
+	}
+}
